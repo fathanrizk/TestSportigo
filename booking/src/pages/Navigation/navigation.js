c30faf59ff7d6c01f72fd5426daaf4168898b0ba
febiolagatha <febiolagatha@gmail.com>
2020-06-24 12:35:48 +0700

    Update Navigation

--- booking/src/pages/Navigation/navigation.js
+++ booking/src/pages/Navigation/navigation.js
@@ -2,34 +2,47 @@ import React, {Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {createAppContainer} from 'react-navigation'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Court from './Court';
+import Profile from '.Profile';
 
-const useStyles = makeStyles({
-  root: {
-    flexContainer: {
-      display: 'flex',
-      flexWrap: 'nowrap',
-    },
-    width: 300,
-    height: 80,
-    backgroundColor: "#FFFFFF"
-  },
-});
-
-export default function BookingBottomNavigation() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction label="Court" />
-      <BottomNavigationAction label="Profile"  />
-    </BottomNavigation>
-  );
+class Court extends React.Component {
+  render() {
+    return (
+      <View>
+            <Text>Court</Text>
+      </View>
+    )
+  }
 }
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <View>
+            <Text>Profile</Text>
+      </View>
+    )
+  }
+}
+
+const TabNavigator = createMaterialBottomTabNavigator({
+    Court: {
+      screen: Court,
+    },
+    Profile: {
+      screen: Profile,
+    },
+  },
+  {
+    initialRouteName: 'Court',
+    activeColor: '#FFFFFFF',
+    inactiveColor: '#3e2465',
+    barStyle: { backgroundColor: '#694fad'},
+  }
+);
+
+export default createAppContainer(TabNavigator);
+
